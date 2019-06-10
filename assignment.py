9a9315146812b46c7df1d6c8e8ea3c7b728dd4b2
Phuc Le <phucle.engineer39@gmail.com>
2019-06-10 09:49:03 +0300

    Add first assignment to GitHub

---
+++ assignment.py
@@ -0,0 +1,63 @@
+# 2
+# def Factorial(n):
+#
+#     if (n == 1):
+#         return n
+#     else:
+#         return n * Factorial(n - 1)
+#
+# def Get_input():
+#     n = int(input(" choose your numeber: "))
+#     if n < 0:
+#         print("factorial doesnt exit ")
+#     elif n == 0:
+#         print("the factorial is 1 ")
+#     else:
+#         print("the factorial of ", n, "is", Factorial(n))
+#     return n
+#
+# Get_input()
+
+#1
+# def Print_number(n):
+#     if (n > 7):
+#         print("course passed")
+#     elif (n<5):
+#         print("your number is ", n)
+#         n += 1
+#         return Print_number(n)
+#     else:
+#         print("your number is : ", n)
+#         n += 1
+#         return Print_number(n)
+# def Get_input():
+#     n = int(input("Choose your number "))
+#     if n < 0:
+#         print("n should be between 0 to 10 ")
+#     elif n >10 :
+#         print("n should be between 0 to 10 ")
+#     else:
+#         Print_number(n)
+#     return n
+# Get_input()
+
+
+#3
+# def AbsoluteCount(arr,n):
+#     s =set ()
+#
+#     for i in range (n):
+#         s.add(abs(arr[i]))
+#     return len(s)
+#
+# def Program():
+#     arr = [-2,1,2,0,3,-5,5 ]
+#     n = len(arr)
+#     print("the result is: ", AbsoluteCount(arr,n) )
+#
+# Program()
+
+
+#4
+
+
